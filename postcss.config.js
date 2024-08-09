991b95530d5175370ced4ff08745cf226aea8d43
bimapopo345 <bimapopo345@gmail.com>
2024-08-09 12:59:42 +0700

    first

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
-module.exports = () => ({
-  plugins: [require("autoprefixer"), require("postcss-minify")],
-});
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("postcss-minify"),
+  ],
+};
